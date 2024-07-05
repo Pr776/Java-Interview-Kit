590db8f7dd61d0907d39457c1c8c0d3c59104f93
Pr_77 <73217906+Pr776@users.noreply.github.com>
2024-07-05 21:42:40 +0530

    Delete ArrayPr2.java

--- ArrayPr2.java
+++
@@ -1,9 +0,0 @@
-//write a program to find out wheather a given integer is presented in an array or not.
-public class ArrayPr2 {
-    public static void main(String[] args) {
-      int marks[]={23,56,7,89,100,78};
-      
-    } 
-        
-    }
-
