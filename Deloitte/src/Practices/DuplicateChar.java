c2d8d993ca068a08b4a7a2eda0136fd2aea797d4
pr776 <priyankasamal601@gmail.com>
2024-07-22 11:06:03 +0530

    added find duplicate charcter

--- Deloitte/src/Practices/DuplicateChar.java
+++ Deloitte/src/Practices/DuplicateChar.java
@@ -2,7 +2,32 @@ package Practices;
 
 //find duplicate characters in a strings
 
+import java.util.Scanner;
+
 public class DuplicateChar {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str1=scanner.nextLine();
+//        String str1="Java Developer";
+        char[] str2=str1.toCharArray();
+        int count=0;
+        System.out.println("Duplicate character");
+        for(int i=0; i< str2.length;i++){
+            count=1;
+            for(int j=i+1;j< str2.length;j++){
+                if(str2[i] ==str2[j] && str2[i]!=0){
+                    count++;
+                    str2[j]=0;
+                }
+            }
+            if(count>1 && str2[i]!=0){
+                System.out.println(str2[i]);
+            }
+        }
+
+
+    }
 
 
 }
