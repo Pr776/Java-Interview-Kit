f70c68a7cedf53d330c18053eb5a73823a070b64
pr776 <priyankasamal601@gmail.com>
2024-09-04 11:09:55 +0530

    added jar file

--- PaymentFunctionality/src/main/java/payment/service/PaymentService.java
+++ PaymentFunctionality/src/main/java/payment/service/PaymentService.java
@@ -1,4 +1,9 @@
 package payment.service;
 
+import payment.entity.Payment;
+import payment.payload.PaymentCreateRequest;
+
 public interface PaymentService {
+
+    Payment createPayment(PaymentCreateRequest paymentCreateRequest);
 }
